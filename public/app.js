4950e207974a190a576431f414bcca51677354c8
Dylan <dylanlctll1@gmail.com>
2026-05-10 20:31:05 +0200

    fix supabase

--- public/app.js
+++ public/app.js
@@ -1,19 +1,25 @@
-window.login = async function () {
-    const email = document.getElementById('email').value
-    const password = document.getElementById('password').value
-  
-    const { data, error } = await supabase.auth.signInWithPassword({
-      email,
-      password
-    })
-  
-    console.log("DATA:", data)
-    console.log("ERROR:", error)
-  
-    if (error) {
-      document.getElementById('error').innerText = "Identifiants incorrects"
-    } else {
-      alert("Connexion réussie 🔥")
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-    }
+const supabase = createClient(
+  'https://xxxxx.supabase.co',
+  'sb_publishable_NRgY-vUIROvY0pZ_Zmy_xg_dX5Pucbh'
+)
+
+window.login = async function () {
+  const email = document.getElementById('email').value
+  const password = document.getElementById('password').value
+
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password
+  })
+
+  console.log("DATA:", data)
+  console.log("ERROR:", error)
+
+  if (error) {
+    document.getElementById('error').innerText = "Identifiants incorrects"
+  } else {
+    alert("Connexion réussie 🔥")
   }
+}
