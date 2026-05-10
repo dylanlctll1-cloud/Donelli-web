c0713ea989de926684e2e18819c4150fe21ee9ec
Dylan <dylanlctll1@gmail.com>
2026-05-10 20:08:45 +0200

    fix login

--- public/app.js
+++ public/app.js
@@ -1,12 +1,5 @@
-console.log("JS CHARGÉ");
-import { createClient } from 'https://esm.sh/@supabase/supabase-js'
-
-const supabase = createClient(
-  'https://finflxwozcdhomynmcfo.supabase.co',
-  'sb_publishable_NRgY-vUIROvY0pZ_Zmy_xg_dX5Pucbh'
-)
 window.login = async function () {
-    const email = document.getElementById('email').value
+    const email = document.getElementById('username').value
     const password = document.getElementById('password').value
   
     const { data, error } = await supabase.auth.signInWithPassword({
@@ -14,9 +7,11 @@ window.login = async function () {
       password
     })
   
+    console.log("DATA:", data)
+    console.log("ERROR:", error)
+  
     if (error) {
-      document.getElementById('error').innerText = error.message
-      console.log(error)
+      document.getElementById('error').innerText = "Identifiants incorrects"
     } else {
       alert("Connexion réussie 🔥")
     }
